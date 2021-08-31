a7d1e05c2f9a6fe46118c907b8d6801e0d86f086
ducvv <ducvv@ames.edu.vn>
2021-08-31 10:20:01 +0700

    update form assignment

---
+++ Form-Assigment/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  subscriptions = ['Basic', 'Advanced', 'Pro'];
+  selectedSubscription = 'Advanced';
+  @ViewChild('userFrm', { static: false }) userFrm: NgForm;
+  onSubmit() {
+    console.log(this.userFrm.value);
+  }
+}
