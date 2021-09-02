6452698bc501b2fc98ce7fc2ccfe3958a9b6b917
ducvv <ducvv@ames.edu.vn>
2021-09-02 11:22:28 +0700

    update code form active

--- Form-Active/karma.conf.js
+++ Form-Active/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/Forms-Reactive'),
+      dir: require('path').join(__dirname, './coverage/Form-Active'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- Form-Active/src/app/app.component.spec.ts
+++ Form-Active/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Forms-Reactive'`, () => {
+  it(`should have as title 'Form-Active'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Forms-Reactive');
+    expect(app.title).toEqual('Form-Active');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Forms-Reactive app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Form-Active app is running!');
   });
 });
